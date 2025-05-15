98ba04ca6d315e99587bb2abf2d504b0963fbe78
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-05-15 18:58:50 -0300

    Feat: Ajustando responsividade do extrato]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import NovaTransacao from "../components/Nova-transacao";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-6">
+    <div className="flex flex-col md:flex-row gap-6">
       <div className="flex flex-col gap-6">
         <Card />
         <NovaTransacao />
