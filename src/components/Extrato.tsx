332af990cb2e926a66d3a17a41eaac49db7ac045
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-05-14 22:35:06 -0300

    Feat: Extrato

--- src/components/Extrato.tsx
+++ src/components/Extrato.tsx
@@ -1,7 +1,25 @@
 export function Extrato() {
   return (
-    <div className="card bg-white shadow-md rounded-lg flex flex-row items-center justify-center flex-1 min-w-20">
-      <h2 className="title">Extrato </h2>
+    <div className="card bg-white shadow-md rounded-lg flex flex-row items-start justify-center flex-1 min-w-20">
+      <aside className="extrato">
+        <h3 className="title font-semibold text-preto pb-4">Extrato</h3>
+        <div className="registro-transacoes">
+          <div className="transacoes-group">
+            <p className="mes-group">Novembro</p>
+            <p className="transacao-item">
+              <span>Depósito</span>
+              <h6>R$ 150</h6>
+            </p>
+          </div>
+          <div className="transacoes-group">
+            <p className="mes-group">Novembro</p>
+            <p className="transacao-item">
+              <span>Depósito</span>
+              <h6>R$ 150</h6>
+            </p>
+          </div>
+        </div>
+      </aside>
     </div>
   );
 }
