4947607177d9e094b7bc4715dfd43c4147c155dd
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-05-14 22:37:14 -0300

    Feat: Extrato ajuste de tamanbo

--- src/components/Extrato.tsx
+++ src/components/Extrato.tsx
@@ -1,6 +1,6 @@
 export function Extrato() {
   return (
-    <div className="card bg-white shadow-md rounded-lg flex flex-row items-start justify-center flex-1 min-w-20">
+    <div className="card bg-white shadow-md rounded-lg flex flex-row  min-w-40">
       <aside className="extrato">
         <h3 className="title font-semibold text-preto pb-4">Extrato</h3>
         <div className="registro-transacoes">
@@ -12,7 +12,7 @@ export function Extrato() {
             </p>
           </div>
           <div className="transacoes-group">
-            <p className="mes-group">Novembro</p>
+            <p className="mes-group">Dezembro</p>
             <p className="transacao-item">
               <span>Depósito</span>
               <h6>R$ 150</h6>
